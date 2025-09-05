4c14b603b03775392aa4a034a8e4090500dea4ad
4shey <tholiulasbah18@gmail.com>
2025-09-05 17:09:23 +0700

    koding tipis

--- flutter_app_test/lib/pages/login_page.dart
+++ flutter_app_test/lib/pages/login_page.dart
@@ -6,51 +6,110 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset('assets/images/lojo.png', width: 368, height: 368),
-            SizedBox(height: 15),
-            Text(
-              'Sign Up',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            SizedBox(height: 33),
-            ElevatedButton(
-              onPressed: () {
-                // Aksi ketika tombol ditekan
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue, // Warna latar tombol
-                foregroundColor: Colors.white, // Warna teks & ikon
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                    12,
-                  ), // Radius sudut tombol
-                ),
-                padding: EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 12,
-                ), // Opsional
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/lojo.png', width: 163, height: 163),
+              SizedBox(height: 15),
+              Text(
+                'Sign Up',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
+              SizedBox(height: 33),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF07BEB8),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 19, vertical: 19),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.login),
+                      SizedBox(width: 55),
+                      Text(
+                        'Sign Up With Google',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      SizedBox(width: 58),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 27),
+              Row(
                 children: [
-                  Icon(Icons.login), // Ikon
-                  SizedBox(width: 8), // Jarak antara ikon & teks
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey[300],
+                      endIndent: 10,
+                    ),
+                  ),
+                  SizedBox(width: 22),
                   Text(
-                    'Masuk', // Teks tombol
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors
-                          .white, // Warna teks (opsional, bisa di styleFrom juga)
+                    'Or',
+                    style: TextStyle(color: Colors.grey, fontSize: 15),
+                  ),
+                  SizedBox(width: 22),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Colors.grey[300],
+                      indent: 10,
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(height: 27),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFFFFF),
+                    foregroundColor: Color(0xFF07BEB8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      side: BorderSide(
+                        color: Color(0xFF07BEB8), // Warna border
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 19, vertical: 19),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.login),
+                      SizedBox(width: 55),
+                      Text(
+                        'Sign Up With Number Hp',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      SizedBox(width: 58),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
